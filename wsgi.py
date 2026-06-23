8137da85caa61cff212e38a68eaf506a6ad2fff4
akaufman <akaufman@nrao.edu>
2026-06-23 09:20:21 -0400

    Fixed some .env things

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for prototype_project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / '.env')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+application = get_wsgi_application()
