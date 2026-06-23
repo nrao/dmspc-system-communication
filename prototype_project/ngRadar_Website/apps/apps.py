f2c795914cca5a95626482338026c9dbbe27c041
akaufman <akaufman@nrao.edu>
2026-06-23 10:03:00 -0400

    Removed nested prototype_project folder.

--- prototype_project/ngRadar_Website/apps/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NgradarWebAppConfig(AppConfig):
-    name = 'ngRadar_Website'
--- prototype_project/ngRadar_Website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import views
-
-
-urlpatterns = [
-    # Maps the root URL of this app to the index view function
-    path('', views.index, name='index'),
-]
