d5ecbbaefaafdbff158bb7a945dbb838086a2d47
tnguyen-nrao <tnguyen@nrao.edu>
2026-08-07 12:18:29 -0400

    experiement with stat Makefile

---
+++ experiment/create_file.py
@@ -0,0 +1,17 @@
+import time
+import random
+import os 
+
+file_name = "some_file.txt"
+chunk_size_mb = 10 
+num_chunks = 10
+
+with open(file_name, "wb") as file:
+    for _ in range(num_chunks):
+        buffer = random.randbytes(chunk_size_mb * 1024 * 1024)
+        file.write(buffer)
+        current = os.stat(file_name).st_size
+        print(f"(GROWING ...{current} bytes)")
+        time.sleep(1)
+
+print("file is completed..")
---
+++ experiment/poller.py
@@ -0,0 +1,31 @@
+import os
+import time 
+
+file_path = "some_file.txt"
+last_size = None 
+
+while True:
+    current_size = os.stat(file_path).st_size
+    if last_size is None:
+        last_size = current_size
+    elif last_size != current_size:
+        last_size = current_size
+        print(f"(GROWING ...{last_size} bytes)")
+    else:
+        count = 0 
+        while count < 5:
+            current_size = os.stat(file_path).st_size
+            if last_size != current_size:
+                last_size = current_size
+                print(f"(GROWING ...{last_size} bytes)")
+                count = 0 
+            else:
+                count += 1 
+            time.sleep(1)
+        if last_size == current_size:
+            print("COMPLETE")
+            break
+
+    time.sleep(1)
+
+
