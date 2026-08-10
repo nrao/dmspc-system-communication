9b1426d66bd78d58e1138cc613263569de0f0667
Nicole DePergola <281976283+ndepergo@users.noreply.github.com>
2026-08-10 19:10:40 -0400

    not working

--- ngRadar_Website/management/commands/etr_progress_writer.py
+++ ngRadar_Website/management/commands/etr_progress_writer.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 import uuid
 from django.core.management.base import BaseCommand
-from ngRadar_Website.models.models import gbtEvent, dsocEvent, ETransferEvent
+from ngRadar_Website.models.models import ETransferEvent
 from ngRadar_Website.enums import Stations, Status
 from ngRadar_Website.utils import latency_calc, bootstrap, consume, write_transfer_progress
 from pathlib import Path
@@ -11,53 +11,60 @@ import time
 
 
 etd_path = Path("/dsoc/incoming")
-# add the actual filename that comes from the ETransferEvent table
-
-
-def file_size_bytes(p):
-    return p.stat().st_size  # size in bytes
-
-size_in_bytes = file_size_bytes(etd_path)
-print(size_in_bytes)
 
 
 def process_msg(msg, producer_topic=None, producer_config=None):
     payload = json.loads(msg.value().decode("utf-8"))
-
     gbt_uuid = uuid.UUID(payload["gbt_uuid"])
+    #print(gbt_uuid)
 
     while True:
-        if not ETransferEvent.objects.filter(gbt_uuid=gbt_uuid, status__in=[Status.READY],).exists():
-            time.sleep(0.5)
-            continue
+        while True:
+            etransfer_data = (
+            ETransferEvent.objects
+            .filter(gbt_uuid=gbt_uuid, status=4)
+            .values_list("transfer_uuid", "num_bytes")
+            .first()
+            )
 
-        try:
-            etransfer_data = ETransferEvent.objects.filter(gbt_uuid=gbt_uuid, status__in=[Status.READY],).values_list('transfer_uuid', 'num_bytes').first()
-
-            transfer_uuid = etransfer_data[0]
-            total_bytes = etransfer_data[1]
-
-            incoming_file = etd_path / f"{transfer_uuid}.bin"
-
-            while True:
-
-                received_bytes = incoming_file.stat().st_size
-                percent = f"{(received_bytes / total_bytes * 100):.1f}"
-
-                write_transfer_progress(received_bytes=received_bytes, total_bytes=total_bytes, percent=percent, transfer_id=transfer_uuid)
+            if etransfer_data is None:
                 time.sleep(0.5)
+            else:
+                transfer_uuid, num_bytes = etransfer_data
+                break
 
-                if received_bytes == total_bytes:
-                    break
+        incoming_file = etd_path / f"{transfer_uuid}.bin"
+        #print(incoming_file)
 
+        if num_bytes == 0:
+            yield process_msg(event="progress_error",
+                                data=json.dumps({"message": "num_bytes is 0"}))
+            break
 
-        except Exception as e:
-            yield process_msg(event="progress_error", data=json.dumps({"message": str(e)}))
+        while True:
+            if not incoming_file.exists():
+                time.sleep(0.5)
+                continue
 
-        #time.sleep(0.2)
+            received_bytes = incoming_file.stat().st_size
+            percent = (received_bytes / num_bytes * 100)
+
+            write_transfer_progress(
+                received_bytes=received_bytes,
+                total_bytes=num_bytes,
+                percent=f"{percent:.1f}",
+                transfer_id=transfer_uuid,
+            )
+            time.sleep(0.5)
+
+            if received_bytes >= num_bytes:
+                break
+
         break
 
-        
+
+
+
 
 
 
@@ -70,6 +77,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Poop")
 
-        topic, config = bootstrap(Stations.DSOC)
+        topic, config = bootstrap(Stations.ETR)
 
         consume(topic, config, process_msg)
